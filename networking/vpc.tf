f4d20863e94d4ede81098d3d13feddde1a0018b3
sanket-turing <sanket.s@turing.com>
2026-06-10 16:55:03 +0530

    feat: aws three tier architecture

---
+++ networking/vpc.tf
@@ -0,0 +1,78 @@
+resource "aws_vpc" "main" {
+
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "3tier-vpc"
+  }
+}
+
+resource "aws_subnet" "public_a" {
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-b"
+  }
+}
+
+resource "aws_subnet" "private_app_a" {
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-app-a"
+  }
+}
+
+resource "aws_subnet" "private_app_b" {
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-app-b"
+  }
+}
+
+resource "aws_subnet" "private_db_a" {
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-db-a"
+  }
+}
+
+resource "aws_subnet" "private_db_b" {
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-db-b"
+  }
+}
