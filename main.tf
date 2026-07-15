b7da6885a9742bf7a94386e50153496402bbb423
Durgesh <durgeshdhamane123@gmail.com>
2026-07-15 15:56:26 +0530

    added EKS cluster

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ module "security" {
 
   vpc_id = module.networking.vpc_id
 }
+
+module "eks" {
+  source = "./modules/eks"
+
+  cluster_name = "three-tier-eks"
+
+  subnet_ids = [
+    module.networking.private_app_subnet_a,
+    module.networking.private_app_subnet_b
+  ]
+}
